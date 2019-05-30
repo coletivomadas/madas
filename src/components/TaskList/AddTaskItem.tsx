e2f127135f44966833e297e60f7b3d19460202da
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2019-05-30 20:08:29 -0300

    feat(component): handle focused behavior in input
    - Handle focused behavior and style for AddTaskItem

--- src/components/TaskList/AddTaskItem.tsx
+++ src/components/TaskList/AddTaskItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Icon, Input, ListItem } from 'native-base';
+import styles from './styles';
 
 interface IProps {
   onChangeText: (text: string) => void;
@@ -9,13 +10,25 @@ interface IProps {
 }
 
 const AddTaskItem = (props: IProps) => {
+  const [isFocused, useSetFocus] = React.useState(false);
   const { placeholder, value, onChangeText, onSubmitEditing } = props;
   return (
     <ListItem>
       <Button transparent>
-        <Icon active name="add" />
+        <Icon
+          active
+          name="add"
+          style={[styles.addTaskItemIcon, !isFocused ? styles.addItemInactiveIcon : styles.addItemActiveIcon]}
+        />
       </Button>
-      <Input placeholder={placeholder} value={value} onSubmitEditing={onSubmitEditing} onChangeText={onChangeText} />
+      <Input
+        onBlur={() => useSetFocus(false)}
+        onFocus={() => !isFocused && useSetFocus(true)}
+        placeholder={!isFocused ? placeholder : ''}
+        value={value}
+        onSubmitEditing={onSubmitEditing}
+        onChangeText={onChangeText}
+      />
     </ListItem>
   );
 };
