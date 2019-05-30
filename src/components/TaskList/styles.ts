4021941f7516d334bbc04df12f95017b3a8bc11d
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2019-05-30 20:06:03 -0300

    style(components): initial settings for addTaskItem

---
+++ src/components/TaskList/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  addTaskItemIcon: {
+    fontSize: 28,
+  },
+  addItemInactiveIcon: {
+    color: '#969699',
+  },
+  addItemActiveIcon: {
+    color: '#000000',
+  },
+});
