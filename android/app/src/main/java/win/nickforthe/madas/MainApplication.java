79b83ba548ccf5643f6cd0efebae6287978af6f7
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2019-05-31 20:41:09 -0300

    chore: link react-native-gesture-handler

--- android/app/src/main/java/win/nickforthe/madas/MainApplication.java
+++ android/app/src/main/java/win/nickforthe/madas/MainApplication.java
@@ -3,6 +3,7 @@ package win.nickforthe.madas;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNGestureHandlerPackage(),
           new RNFirebasePackage(),
           new RNFirebaseMessagingPackage(),
           new RNFirebaseFirestorePackage()
