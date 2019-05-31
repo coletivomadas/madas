66a264d0259d551b8913f0014f4d69d210ad1551
Nícolas Deçordi dos Reis <6071319+eowfenth@users.noreply.github.com>
2019-05-30 21:29:17 -0300

    feat(tasks): implement add new task component (#9)
    * feat(tasks): add new component
    
    - Initial implementation for 'add new task item' component
    
    * style(components): initial settings for addTaskItem
    
    * feat(component): handle focused behavior in input
    
    - Handle focused behavior and style for AddTaskItem
    
    * style(component): adjust paddings for AddTaskItem
    
    * feat(component): wrap AddTaskItem around a Card
    
    * feat(component): adjust behavior when focusing AddTaskItem
    
    * docs(component): AddTaskItem with todos

--- src/components/TaskList/AddTaskItem.tsx
+++ src/components/TaskList/AddTaskItem.tsx
@@ -1,31 +1,46 @@
 import React from 'react';
-import {} from 'native-base';
+import { Button, Card, Icon, Input, ListItem } from 'native-base';
+import styles from './styles';
 
-const AddTaskItem = () => {
+interface IProps {
+  onChangeText: (text: string) => void;
+  onSubmitEditing: () => void;
+  placeholder: string;
+  value: string;
+}
+
+/**
+ * Add new a Task component
+ *
+ * @todo Focus Input when pressing/focusing the Card itself;
+ * @todo Animate the Icon to change its color when being focused;
+ * @todo Animate the Text to fade out in bottom of container when submiting;
+ */
+const AddTaskItem = (props: IProps) => {
+  const [isFocused, useSetFocus] = React.useState(false);
+  const { placeholder, value, onChangeText, onSubmitEditing } = props;
   return (
-    <ListItem>
-      <Button transparent>
-        <Icon active name="add" />
+    <Card style={styles.addItemContainer}>
+      <ListItem style={styles.addItemSubContainer} onFocus={() => !isFocused && useSetFocus(true)}>
+        <Button transparent onFocus={() => !isFocused && useSetFocus(true)}>
+        <Icon
+          active
+          name="add"
+            style={[styles.addItemIcon, !isFocused ? styles.addItemInactiveIcon : styles.addItemActiveIcon]}
+        />
       </Button>
       <Input
-        placeholder="Adicione uma nova tarefa..."
-        value={this.state.input}
-        onSubmitEditing={() =>
-          this.setState({
-            data: [
-              {
-                id: (this.state.data.length + 1).toString(),
-                title: this.state.input,
-                isDeleted: false,
-                isDone: false,
-              },
-              ...this.state.data,
-            ],
-            input: '',
-          })
-        }
-        onChangeText={text => this.setState({ input: text })}
+        onBlur={() => useSetFocus(false)}
+          blurOnSubmit={false}
+        onFocus={() => !isFocused && useSetFocus(true)}
+        placeholder={!isFocused ? placeholder : ''}
+        value={value}
+        onSubmitEditing={onSubmitEditing}
+        onChangeText={onChangeText}
       />
     </ListItem>
+    </Card>
   );
 };
+
+export default AddTaskItem;
---
+++ src/components/TaskList/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  addItemContainer: {},
+  addItemSubContainer: {
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+  addItemIcon: {
+    fontSize: 28,
+  },
+  addItemInactiveIcon: {
+    color: '#969699',
+  },
+  addItemActiveIcon: {
+    color: '#000000',
+  },
+});
