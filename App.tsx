a342780238da324c160b410fd5939654d7de3a4c
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2019-05-31 18:22:25 -0300

    feat: rearrange App to use native-base and firebase mount

--- App.tsx
+++ App.tsx
@@ -1,40 +1,42 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import { Container, Content, Text, StyleProvider } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
+import firebase from 'react-native-firebase';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends React.Component {
+  componentDidMount = async () => {
+    const fcmToken = await firebase.messaging().getToken();
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      // user has permissions
+    } else {
+      // user doesn't have permission
+    }
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+    } catch (error) {
+      // User has rejected permissions
+    }
+    if (fcmToken) {
+      // user has a device token
+      // this.onTokenRefreshListener = firebase.messaging().onTokenRefresh(fcmToken => {
+      //   // Process your token as required
+      // });
+    } else {
+      // user doesn't have a device token yet
+    }
+  };
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <StyleProvider style={getTheme(material)}>
+        <Container>
+        </Container>
+      </StyleProvider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
