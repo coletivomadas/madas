eae4334d92d9259349e5b1195a29b1ff70e6f192
Nícolas Deçordi dos Reis <6071319+eowfenth@users.noreply.github.com>
2019-05-31 20:45:09 -0300

    feature: add navigation settings (#15)
    * chore(package): add react-navigation@3.11.0
    
    * chore(package): add react-native-gesture-handler@1.2.2
    
    * chore: link react-native-gesture-handler
    
    * chore: link react-native-gesture-handler to iOS
    
    * chore: add google-services.json to .gitignore
    
    * style: adjust TaskScreen header size and positions using flex
    
    * feat: add AppNavigator in App

--- src/containers/TaskScreen/styles.ts
+++ src/containers/TaskScreen/styles.ts
@@ -6,16 +6,16 @@ export default StyleSheet.create({
   headerBackground: {
     position: 'absolute',
     backgroundColor: '#CBD0FF',
-    height: deviceHeight * 0.15 + 34 + 5,
+    height: deviceHeight * 0.2 + 34 + 5,
     width: deviceWidth,
   },
   header: {
-    height: deviceHeight * 0.15,
+    height: deviceHeight * 0.2,
+    justifyContent: 'center',
   },
   headerContent: {
     color: '#122930',
     fontSize: 28,
-    marginTop: 40,
   },
   content: {
     paddingHorizontal: 20,
