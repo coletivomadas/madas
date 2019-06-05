303438e35f005170406324ae3962dfd9110748c6
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2019-06-05 20:39:27 -0300

    refactor(App): set switch navigator to future login state handler

--- App.tsx
+++ App.tsx
@@ -3,17 +3,44 @@ import { Container, StyleProvider } from 'native-base';
 import getTheme from './native-base-theme/components';
 import material from './native-base-theme/variables/material';
 import TaskScreen from './src/containers/TaskScreen';
+import Home from './src/containers/Home';
 import firebase from 'react-native-firebase';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
-const AppNavigator = createStackNavigator({
+/**
+ * @todo create keystore
+ * @todo integrate with CodePush
+ * @todo integrate with Fastlane
+ * @todo complete Readme
+ * @todo integrate with HockeyApp
+ * @todo login screen
+ * @todo login integration with Firebase
+ */
+const MainNavigator = createStackNavigator({
   Home: {
+    screen: Home,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  TaskScreen: {
     screen: TaskScreen,
     navigationOptions: {
       header: null,
     },
   },
 });
+
+const AppNavigator = createSwitchNavigator(
+  {
+    Pending: {},
+    Auth: {},
+    Home: MainNavigator,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
