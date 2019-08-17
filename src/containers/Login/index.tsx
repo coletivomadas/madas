779f6a50b644c5044f099c3c7cd00730137a2bc3
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2019-08-16 21:03:45 -0300

    fix(container): Login to have better QoL

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -3,14 +3,35 @@ import { Dimensions, Image, StyleSheet, View } from 'react-native';
 import { Content, Button, Icon, Input, Item, Spinner, Text } from 'native-base';
 import { NavigationScreenComponent, NavigationScreenProps, NavigationStackScreenOptions } from 'react-navigation';
 import firebase from 'react-native-firebase';
+import { string } from 'prop-types';
 
 interface IProps extends NavigationScreenProps<{}> { }
-
+interface IState {
+  isLoading?: boolean;
+  email?: string;
+  password?: string;
+}
 const { height: deviceHeight, width: deviceWidth } = Dimensions.get('window');
 const Login: NavigationScreenComponent = (props: IProps) => {
   const emailRef = useRef(null);
   const passwordRef = useRef<any>(null);
-  const [state, setState] = useState({ email: '', password: '' });
+  const [state, setState] = useState<IState>({ isLoading: false, email: '', password: '' });
+  const handleLogin = async () => {
+    const { email, password } = state;
+    if (!email || !password) {
+      return;
+    }
+
+    setState({ isLoading: true });
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email!, password!)
+      .then(() => {
+        props.navigation.navigate('Main');
+      })
+      .catch(error => setState({ isLoading: false }));
+  };
   return (
     <Content style={styles.scrollContainer} contentContainerStyle={styles.container}>
       <View style={styles.logoContainer}>
@@ -18,6 +39,7 @@ const Login: NavigationScreenComponent = (props: IProps) => {
       </View>
       <Item regular style={styles.item}>
         <Input
+          autoCapitalize={'none'}
           placeholder="Seu E-mail"
           returnKeyType={'next'}
           ref={emailRef}
@@ -34,12 +56,14 @@ const Login: NavigationScreenComponent = (props: IProps) => {
           placeholder="Password"
           onChangeText={input => setState({ email: state.email, password: input })}
           value={state.password}
+          secureTextEntry={true}
+          autoCapitalize={'none'}
           returnKeyType={'done'}
           ref={passwordRef}
         />
       </Item>
-      <Button block style={styles.loginButton}>
-        <Text>LOGIN</Text>
+      <Button block style={styles.loginButton} onPress={handleLogin}>
+        {!state.isLoading ? <Text>LOGIN</Text> : <Spinner color="#FFFFFF" />}
       </Button>
     </Content>
   );
